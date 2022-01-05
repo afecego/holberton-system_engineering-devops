dcb6c469bac22bbf4934e32acfc1ac58f7e32645
afecego <afecego@gmail.com>
2022-01-04 23:43:37 -0500

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # SSH configuration file for the local SSH client whit puppet
 file_line { 'Declare_identity_file':
     ensure => created,
-    path   => '/etc/ssh/ssh_config'
+    path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn_off_passwd_auth':
     ensure => created,
-    path   => '/etc/ssh/ssh_config'
+    path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
 }
