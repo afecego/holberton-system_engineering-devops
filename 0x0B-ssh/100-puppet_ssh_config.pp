08ae42ab7b667920af15d537469d7e97d6719e42
afecego <afecego@gmail.com>
2022-01-04 23:38:33 -0500

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line { 'Turn_off_passwd_auth':
     ensure => created,
     path   => '/etc/ssh/ssh_config'
-    line   => '    PasswordAuthentication no',
+    line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare_identity_file':
     ensure => created,
     path   => '/etc/ssh/ssh_config'
-    line   => '    IdentityFile ~/.ssh/school',
+    line   => 'IdentityFile ~/.ssh/school',
 }
