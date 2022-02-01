696226584b19f67c237c4cf3cda13fc93833a60f
afecego <afecego@gmail.com>
2022-01-31 21:15:45 -0500

    Correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,22 +7,23 @@ import sys
 
 if __name__ == "__main__":
 
+    arg = int(sys.argv[1])
     url = "https://jsonplaceholder.typicode.com"
     direct = "/todos"
     us = "/users"
-    cod = "?userId={}".format(int(sys.argv[1]))
+    cod = "?userId={}".format(arg)
     web_all = requests.get(url + direct + cod).json()
-    web_user = requests.get(url + us + "/" + int(sys.argv[1])).json()
+    web_user = requests.get(url + us + "/{}".format(arg)).json()
 
     data_title = []
 
     for task in web_all:
-        data_title.append([int(sys.argv[1]), web_user.get("username"),
+        data_title.append([arg, web_user.get("username"),
                           task.get("completed"), task.get("title")])
 
-    file_cvs = str(sys.argv[1]) + ".cvs"
+    file_cvs = "{}.cvs".format(arg)
 
-    with open(file_cvs, mode='w', newline="") as csv_file:
+    with open(file_cvs, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL)
         writer.writerows(data_title)
