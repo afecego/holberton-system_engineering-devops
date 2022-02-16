f4d86934aa7fe5584ce65419636a799b0fa71687
afecego <afecego@gmail.com>
2022-02-15 22:09:26 -0500

    Correction style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,15 @@ def number_of_subscribers(subreddit):
     """
     get data of numbers of subscriptor
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    if subreddit is None:
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     moz = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64)'
     app = 'AppleWebKit/537.36 (KHTML, like Gecko)'
     chro = 'Chrome/75.0.3770.100 Safari/537.36'
     agen = moz + ' ' + app + ' ' + chro
     header = {'User-Agent': agen}
-
-    if subreddit is None:
-        return 0
 
     web_all = requests.get(url, headers=header, allow_redirects=False)
     if web_all.status_code >= 300:
