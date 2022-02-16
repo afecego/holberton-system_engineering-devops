8b66aaa02d3fb52406fd27b7ec9c9d66e3dd0e50
afecego <afecego@gmail.com>
2022-02-15 22:06:03 -0500

    Correction style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and
+"""
+function that queries the Reddit API and
 returns the number of subscribers
 """
 import requests
@@ -9,17 +10,17 @@ def number_of_subscribers(subreddit):
     """
     get data of numbers of subscriptor
     """
-    url = "https://www.reddit.com/"
-    path = f"r/{subreddit}/about.json"
-    web = url + path
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     moz = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64)'
     app = 'AppleWebKit/537.36 (KHTML, like Gecko)'
     chro = 'Chrome/75.0.3770.100 Safari/537.36'
     agen = moz + ' ' + app + ' ' + chro
     header = {'User-Agent': agen}
-    print(header)
-    web_all = requests.get(web, headers=header, allow_redirects=False)
 
+    if subreddit is None:
+        return 0
+
+    web_all = requests.get(url, headers=header, allow_redirects=False)
     if web_all.status_code >= 300:
         return (0)
     return (web_all.json().get('data').get('subscribers'))
