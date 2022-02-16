b94a5cab4a9d59572e91ddb26f236076ce734ad8
afecego <afecego@gmail.com>
2022-02-15 20:05:24 -0500

    Correction style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """function that queries the Reddit API and
-returns the number of subscribers"""
+returns the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """get data of numbers of subscriptor"""
+    """
+    get data of numbers of subscriptor
+    """
     url = "https://www.reddit.com/"
     path = f"r/{subreddit}/about.json"
     web = url + path
