69e1b4ca490b1e81a7ee671a01b281020fce8c56
afecego <afecego@gmail.com>
2022-02-15 22:46:37 -0500

    Title of data of web page

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,20 +8,20 @@ import requests
 def top_ten(subreddit):
     """Print top 10 posts"""
     if subreddit is None:
-        print('None')
-    dicc = {}
+        print("None")
+
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64)\
-        AppleWebKit/537.36 (KHTML, like Gecko)\
-        Chrome/75.0.3770.100 Safari/537.36'
-        }
+    moz = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64)'
+    app = 'AppleWebKit/537.36 (KHTML, like Gecko)'
+    chro = 'Chrome/75.0.3770.100 Safari/537.36'
+    agen = moz + ' ' + app + ' ' + chro
+    header = {'User-Agent': agen}
 
     web_all = requests.get(url, headers=header, allow_redirects=False)
-    if web_all.status_code >= 300:
-        print('None')
-    else:
+    if web_all.status_code == 200:
         info = web_all.json().get('data').get('children')
         for i in info[0:10]:
             dicc = i.get('data').get('title')
             print(dicc)
+    else:
+        print("None")
