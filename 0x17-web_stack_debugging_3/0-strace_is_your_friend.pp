b244ad77ebf2741153f4a4a8fed174f1f14f530d
afecego <afecego@gmail.com>
2022-02-16 21:14:45 -0500

    Fix the path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix file of wp
 file_line {'fix-wp':
-  command   => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path      => ':/usr/share/php:/usr/share/pear',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ':/usr/share/php:/usr/share/pear',
 }
